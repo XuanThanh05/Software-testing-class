cba4c6cdd921b992038df59c042bd8c1fe0955c1
XuanThanh05 <xuanthanhle999@gmail.com>
2026-01-12 00:00:58 +0700

    feat: implement countExcellentStudents() #1

---
+++ unit_test/src/main/java/com/mycompany/unit_test/StudentAnalyzer.java
@@ -0,0 +1,22 @@
+package com.mycompany.unit_test;
+import java.util.List;
+
+public class StudentAnalyzer {
+    public int countExcellentStudents(List<Double> scores) {
+        if (scores == null || scores.isEmpty()) {
+            return 0;
+        }
+
+        int count = 0;
+        for (Double score : scores) {
+            if (score >= 0 && score <= 10) {  
+                if (score >= 8.0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
+}
