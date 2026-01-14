43643c12b818460069d27f8c1329a737efdf5728
XuanThanh05 <xuanthanhle999@gmail.com>
2026-01-14 16:31:45 +0700

    tách mỗi @test một bài test

--- unit_test/src/test/java/com/mycompany/unit_test/StudentAnalyzerTest.java
+++ unit_test/src/test/java/com/mycompany/unit_test/StudentAnalyzerTest.java
@@ -9,17 +9,51 @@ import java.util.Collections;
 public class StudentAnalyzerTest {
 
     @Test
-    public void testCountExcellentStudents() {
+    public void testCountExcellentStudents_NormalList() {
         StudentAnalyzer analyzer = new StudentAnalyzer();
-        assertEquals(2,analyzer.countExcellentStudents(Arrays.asList(9.0, 8.5, 7.0, 11.0, -1.0)));
-        assertEquals(0,analyzer.countExcellentStudents(Collections.emptyList()));
-        assertEquals(1,analyzer.countExcellentStudents(Arrays.asList(10.0, 0.0, 7.5)));
+        assertEquals(2,
+                analyzer.countExcellentStudents(
+                        Arrays.asList(9.0, 8.5, 7.0, 11.0, -1.0)));
     }
+
     @Test
-    public void testCalculateValidAverage() {
+    public void testCountExcellentStudents_EmptyList() {
         StudentAnalyzer analyzer = new StudentAnalyzer();
-        assertEquals(8.17,analyzer.calculateValidAverage(Arrays.asList(9.0, 8.5, 7.0, 11.0, -1.0)),0.01);
-        assertEquals(0,analyzer.calculateValidAverage(Collections.emptyList()));
-        assertEquals(5.0,analyzer.calculateValidAverage(Arrays.asList(0.0, 10.0)),0.01);
+        assertEquals(0,
+                analyzer.countExcellentStudents(Collections.emptyList()));
+    }
+
+    @Test
+    public void testCountExcellentStudents_MixedValues() {
+        StudentAnalyzer analyzer = new StudentAnalyzer();
+        assertEquals(1,
+                analyzer.countExcellentStudents(
+                        Arrays.asList(10.0, 0.0, 7.5)));
+    }
+
+    @Test
+    public void testCalculateValidAverage_NormalList() {
+        StudentAnalyzer analyzer = new StudentAnalyzer();
+        assertEquals(8.17,
+                analyzer.calculateValidAverage(
+                        Arrays.asList(9.0, 8.5, 7.0, 11.0, -1.0)),
+                0.01);
+    }
+
+    @Test
+    public void testCalculateValidAverage_EmptyList() {
+        StudentAnalyzer analyzer = new StudentAnalyzer();
+        assertEquals(0,
+                analyzer.calculateValidAverage(Collections.emptyList()),
+                0.01);
+    }
+
+    @Test
+    public void testCalculateValidAverage_BorderValues() {
+        StudentAnalyzer analyzer = new StudentAnalyzer();
+        assertEquals(5.0,
+                analyzer.calculateValidAverage(
+                        Arrays.asList(0.0, 10.0)),
+                0.01);
     }
 }
